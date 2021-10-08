ccfdc71024adb503bcb56d48d3265fcaf5de0404
Theodore Quinn <tcquinn@tcquinn.org>
2021-10-07 19:18:34 -0700

    Repackage

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# wf_core_data
+# nwea_utils
 
-Python tools for working with Wildflower Schools core data
+Tools for working with NWEA assessments data
+
+## Install
+
+`pip install wf-nwea-utils`
 
 ## Tasks
-* Consider splitting library into atomic libraries for each assessment and data source
-* Update naming of submodules and functions to be more consistent across assessments
-* NWEA: Add ability to handle more detailed data download (separate test event and student files)
+
+* TBD
--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ VERSION = open(os.path.join(BASEDIR, 'VERSION')).read().strip()
 
 # Dependencies (format is 'PYPI_PACKAGE_NAME[>]=VERSION_NUMBER')
 BASE_DEPENDENCIES = [
+    'wf-core-data-python>=1.1.0'
     'pandas>=1.3',
-    'numpy>=1.19',
-    'requests>=2.25',
-    'gspread-pandas>=2.2'
+    'numpy>=1.19'
+    'inflection>=0.5.1'
 ]
 
 # TEST_DEPENDENCIES = [
@@ -25,13 +25,13 @@ BASE_DEPENDENCIES = [
 os.chdir(os.path.normpath(BASEDIR))
 
 setup(
-    name='wf-core-data-python',
+    name='wf-nwea-utils',
     packages=find_packages(),
     version=VERSION,
     include_package_data=True,
-    description='Python tools for working with Wildflower Schools core data',
+    description='Tools for working with NWEA assessments data',
     long_description=open('README.md').read(),
-    url='https://github.com/WildflowerSchools/wf-core-data-python',
+    url='https://github.com/WildflowerSchools/wf-nwea-utils',
     author='Theodore Quinn',
     author_email='ted.quinn@wildflowerschools.org',
     install_requires=BASE_DEPENDENCIES,
